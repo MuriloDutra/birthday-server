c6e937cb3c0de954b026f6d56cdade9468f656b0
MuriloDutra <MuriloDutraProjects@gmail.com>
2021-02-03 16:36:10 -0300

    Creating database

---
+++ src/database/database-model.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE birthdayCamila;
+
+USE birthdayCamila;
+
+CREATE TABLE photos (
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    imageUrl TEXT,
+    englishDescription VARCHAR(70),
+    portugueseDescription VARCHAR(70),
+    photoType VARCHAR(10),
+    approved BOOLEAN DEFAULT 0,
+    highlightImage BOOLEAN DEFAULT 0
+);
+
+CREATE TABLE user (
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    email VARCHAR(70),
+    accountPassword VARCHAR(150)
+);
+
+SELECT * FROM birthdayCamila.user;
+
+INSERT INTO birthdayCamila.user (email, accountPassword) values ('TESTE', 'senhaTest')
