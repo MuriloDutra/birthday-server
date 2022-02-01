1b053b9bf9412f13626818c5aa07583d05c07481
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2022-01-31 22:19:02 -0300

    Updating database

--- src/database/database-model.sql
+++ src/database/database-model.sql
@@ -4,23 +4,21 @@ USE birthdayCamila;
 
 CREATE TABLE photos (
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    imageUrl TEXT,
+    url TEXT,
     englishDescription VARCHAR(70),
     portugueseDescription VARCHAR(70),
     photoType VARCHAR(10),
     approved BOOLEAN DEFAULT 0,
-    highlightImage BOOLEAN DEFAULT 0
+    highlightImage BOOLEAN DEFAULT 0,
+    imageName varchar(32)
 );
 
 CREATE TABLE user (
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     email VARCHAR(70),
-    accountPassword VARCHAR(150)
+    accountPassword VARCHAR(150),
+    token varchar(32)
 );
-
-ALTER TABLE birthdayCamila.user ADD token varchar(32);
-ALTER TABLE birthdayCamila.photos drop column photoType;
-ALTER TABLE birthdayCamila.photos ADD imageName varchar(32);
 
 SELECT * FROM birthdayCamila.user;
 SELECT * FROM birthdayCamila.photos;
